1676a6b2c9f896aa4e139f50f227e0d72cdcf7ca
Langkous <hitschonenberg@outlook.com>
2022-02-13 11:14:05 +0100

    zondag 1

--- template/src/visualization/visualize_simple.py
+++ template/src/visualization/visualize_simple.py
@@ -97,86 +97,6 @@ def roc_curve(fpr: np.ndarray, tpr: np.ndarray) -> None:
     plt.plot([0, 1], [0, 1], "k--")
 
 
-def predictions(
-    X: np.ndarray, y: np.ndarray, model: protocol.GenericModel  # noqa: N803
-) -> None:
-    """Fit a model on X data and y target.
-    generate an equally spaced x-axis between min and max values
-    create a scatterplot of the data and a line of the model.
-
-    Args:
-        X (np.ndarray): data
-        y (np.ndarray): target
-        model ([type]): model with a .fit() function
-    """
-    model.fit(X, y)
-    # make evenly spaced values over the input range
-    newx = np.linspace(min(X), max(X), 100)
-    yhat = model.predict(newx)
-    plt.scatter(X, y, label="data")
-    plt.plot(newx, yhat, label="model")
-    plt.legend()
-
-
-def impact_of_alpha(
-    alphalist: List[float],
-    pipe: pipeline.Pipeline,
-    X: pd.DataFrame,  # noqa: N803
-    y: pd.Series,  # noqa: N803
-) -> None:
-    steps = [*pipe.named_steps]  # this gets the name of the steps
-    model = steps[-1]  # this takes the last step, which is the model
-    for alpha in alphalist:
-        fit_params = {model + "__alpha": alpha}
-        pipe.set_params(**fit_params)  # we set one of the alpha's from the alphaList
-        pipe.fit(X, y)  # and fit the model
-        coef = pipe.named_steps[model].coef_.T  # get the coeficients from the model
-        x = range(len(coef))
-        plt.scatter(x, coef, label=alpha)  # and plot them
-
-    # some esthetics
-    plt.legend(title="alpha")
-    plt.title(model)
-    plt.xticks(x, X.columns, rotation=90)
-    plt.show()
-
-
-def plot_contour(
-    X_train: np.ndarray,  # noqa: N803
-    y_train: np.ndarray,
-    pipe: protocol.GenericModel,
-    granularity: float = 0.1,
-    grid_side: float = 0.5,
-) -> None:
-    X_train = np.array(X_train)  # noqa: N803, N806
-    y_train = np.array(y_train)
-    cm = plt.cm.PiYG  # let's change the colormap.
-
-    # first, we get the min-max range over which we want to plot
-    # this is the area for which we want to know the behavior of the model
-    # we add some extra space with grid_side to the feature space.
-    x0_min, x0_max = X_train[:, 0].min() - grid_side, X_train[:, 0].max() + grid_side
-    x1_min, x1_max = X_train[:, 1].min() - grid_side, X_train[:, 1].max() + grid_side
-    # we make a grid of coordinates
-    xx, yy = np.meshgrid(
-        np.arange(x0_min, x0_max, granularity), np.arange(x1_min, x1_max, granularity)
-    )
-    # and combine the grid into a new dataset.
-    # this new dataset covers (with some granularity)
-    # every point of the original dataset
-    # this newx is equal to the featurespace we want to examine.
-    newx = np.c_[xx.ravel(), yy.ravel()]
-
-    # we make a prediction with the new dataset.
-    # This will show us predictions over the complete featurespace.
-    yhat = pipe.predict(newx)
-
-    # and reshape the prediction, so that it will match our gridsize
-    z = yhat.reshape(xx.shape)
-    plt.contourf(xx, yy, z, cmap=cm, alpha=0.5)
-    data = pd.DataFrame({"x1": X_train[:, 0], "x2": X_train[:, 1]})
-    sns.scatterplot(data=data, x="x1", y="x2", c=y_train, cmap=cm)
-
 
 def gridsearch_heatmap(
     gridresults: GridSearchCV,
@@ -206,27 +126,4 @@ def gridsearch_heatmap(
     sns.heatmap(pivoted, vmin=vmin, vmax=vmax, annot=True)
 
 
-def compare_results(results: Dict[str, Dict[str, float]], ylim: float = None) -> None:
-    """The result is a dictionary with results for training, testing and
-    possibly validation. E.g.:
 
-    >>> results['svm'] = {"train" : pipe.score(X_train, y_train),
-                      "test" : pipe.score(X_test, y_test)}
-
-    The dict will be melted into a format for comparing the results of different models.
-        Args:
-        results (Dict[str, Dict[str, float]]): Dict with
-
-            {"model" :
-                {"datasetname" : 0.8}
-            }
-
-            structure
-
-        ylim ([type], optional): limits y-axis for better plotting. Defaults to None.
-    """
-    logger.info(f"Found results for {len(results.keys())} models")
-    data = pd.DataFrame(results).reset_index()
-    data = data.melt(id_vars="index")
-    sns.barplot(x="index", y="value", hue="variable", data=data)
-    plt.ylim(ylim, 1)
